89f7fe37e436463c93d2cf05f0536d3c5a99ed10
ZYYHsunshine <zzucgg@163.com>
2019-07-14 12:57:48 +0800

    object

---
+++ object.js
@@ -0,0 +1,12 @@
+var user = {
+    name:'John',
+    surname:'Mike'
+  };
+  var fruit = {
+  apple: 20,
+  pear: 20,
+  peach: 10
+  };
+  user.name = 'Peter';
+  console.log(user.name);
+  console.log(fruit.apple+fruit.pear+fruit.peach)

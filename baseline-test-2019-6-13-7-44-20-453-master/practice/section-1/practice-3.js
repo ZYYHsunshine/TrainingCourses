f7c64245eb0f00424f85014399a1af9dfd6cb762
ZYYHsunshine <zzucgg@163.com>
2019-08-19 18:45:21 +0800

    day1

---
+++ baseline-test-2019-6-13-7-44-20-453-master/practice/section-1/practice-3.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function collectSameElements(collectionA, objectB) {
+  var c = new Array();
+  var collectionB=objectB.value;
+  for(var i = 0; i<collectionA.length ; i++)
+  {
+    for(var j = 0; j<collectionB.length ; j++)
+    {
+      if (collectionA[i] == collectionB[j])
+      {
+        c.push(collectionA[i]);
+        break;
+      }
+    }
+  }
+  return c;
+}
---
+++ baseline-test-2019-6-13-7-44-20-453-master/practice/section-1/practice-4.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function collectSameElements(collectionA, objectB) {
+
+  var c = new Array();
+  var collectionB=objectB.value;
+  for(var i = 0; i<collectionA.length ; i++)
+  {
+    for(var j = 0; j<collectionB.length ; j++)
+    {
+      if (collectionA[i].key == collectionB[j]) //取得对象数组中对象的key值
+      {
+        c.push(collectionB[j]);
+        break;
+      }
+    }
+  }
+  return c;
+
+}

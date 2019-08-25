9c6e6a829b2bdf6c7ab87da5cfad7fde0774cc2e
ZYYHsunshine <zzucgg@163.com>
2019-08-25 22:04:35 +0800

    huangmenji

---
+++ take-out-food-1-2019-6-13-7-52-40-729-master/src/items.js
@@ -0,0 +1,19 @@
+function loadAllItems() {
+  return [{
+    id: 'ITEM0001',
+    name: '黄焖鸡',
+    price: 18.00
+  }, {
+    id: 'ITEM0013',
+    name: '黄焖牛',
+    price: 6.00
+  }, {
+    id: 'ITEM0022',
+    name: '黄焖兔',
+    price: 8.00
+  }, {
+    id: 'ITEM0030',
+    name: '黄焖蛇',
+    price: 2.00
+  }];
+}
